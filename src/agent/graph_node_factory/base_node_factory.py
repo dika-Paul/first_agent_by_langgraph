f767313cd3983f23ef5cc924253e284c7b13e03b
dika-Paul <2413657071@qq.com>
2026-04-21 13:57:51 +0800

    创建工厂方法模板基类

---
+++ src/agent/graph_node_factory/base_node_factory.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from langchain_core.runnables import RunnableLambda
+
+
+
+class BaseNodeFactory(ABC):
+    @staticmethod
+    @abstractmethod
+    def return_dict(content: Any) -> dict:
+        pass
+
+
+    @classmethod
+    @abstractmethod
+    def graph_node(cls) -> RunnableLambda:
+        pass
+
+
+
+class BaseSingleCallingNodeFactory(BaseNodeFactory):
+    @staticmethod
+    @abstractmethod
+    def return_dict(content: Any) -> dict:
+        pass
+
+
+    @classmethod
+    @abstractmethod
+    def function(cls):
+        pass
+
+
+    @classmethod
+    def graph_node(cls) -> RunnableLambda:
+        return RunnableLambda(cls.function())
+
+
+
+class BaseMultiCallingNodeFactory(BaseNodeFactory):
+    @staticmethod
+    @abstractmethod
+    def return_dict(content: Any) -> dict:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def sync_function(cls):
+        pass
+
+
+    @classmethod
+    @abstractmethod
+    def async_function(cls):
+        pass
+
+
+    @classmethod
+    def graph_node(cls) -> RunnableLambda:
+        return RunnableLambda(
+            func=cls.sync_function(),
+            afunc=cls.async_function()
+        )
