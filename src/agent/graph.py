9f4a10e441d8ad92376e8af18be8d2eb32cefb05
dika-Paul <2413657071@qq.com>
2026-04-21 15:13:47 +0800

    创建图逻辑

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -5,50 +5,50 @@ Returns a predefined response. Replace logic and configuration as needed.
 
 from __future__ import annotations
 
-from dataclasses import dataclass
-from typing import Any, Dict
+from typing import Literal
 
+from langchain_core.messages import AIMessage
 from langgraph.graph import StateGraph
-from langgraph.runtime import Runtime
-from typing_extensions import TypedDict
+from langgraph.constants import START, END
+
+from graph_state import GraphState, GraphContext
+from graph_node_factory import *
 
 
-class Context(TypedDict):
-    """Context parameters for the agent.
+add_query = AddQueryNodeFactory.graph_node()
+chat_model = ChatModelNodeFactory.graph_node()
+tool_calling = ToolCallNodeFactory.graph_node()
 
-    Set these when creating assistants OR when invoking the graph.
-    See: https://langchain-ai.github.io/langgraph/cloud/how-tos/configuration_cloud/
-    """
+def chat_model_route(state: GraphState) -> Literal['tool_calling', 'chat_model', 'end']:
+    message = state.messages[-1]
 
-    my_configurable_param: str
+    if isinstance(message, AIMessage):
+        tool_calls = message.tool_calls
+        if tool_calls:
+            return 'tool_calling'
 
+        return 'end'
 
-@dataclass
-class State:
-    """Input state for the agent.
-
-    Defines the initial structure of incoming data.
-    See: https://langchain-ai.github.io/langgraph/concepts/low_level/#state
-    """
-
-    changeme: str = "example"
-
-
-async def call_model(state: State, runtime: Runtime[Context]) -> Dict[str, Any]:
-    """Process input and returns output.
-
-    Can use runtime context to alter behavior.
-    """
-    return {
-        "changeme": "output from call_model. "
-        f"Configured with {(runtime.context or {}).get('my_configurable_param')}"
-    }
+    return 'chat_model'
 
 
 # Define the graph
-graph = (
-    StateGraph(State, context_schema=Context)
-    .add_node(call_model)
-    .add_edge("__start__", "call_model")
-    .compile(name="New Graph")
+graph_builder = StateGraph(GraphState, context_schema=GraphContext)
+
+graph_builder.add_node('add_query', add_query)
+graph_builder.add_node('chat_model', chat_model)
+graph_builder.add_node('tool_calling', tool_calling)
+
+graph_builder.add_edge(START, 'add_query')
+graph_builder.add_edge('add_query', 'chat_model')
+graph_builder.add_conditional_edges(
+    source='chat_model',
+    path=chat_model_route,
+    path_map={
+        'chat_model': 'chat_model',
+        'tool_calling': 'tool_calling',
+        'end': END
+    }
 )
+
+graph = graph_builder.compile()
