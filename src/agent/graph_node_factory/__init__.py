c5aa659717849ccae07fd69ad4d61b3293e503a3
dika-Paul <2413657071@qq.com>
2026-04-21 15:13:27 +0800

    修改graph_node_factory的init便于调用

---
+++ src/agent/graph_node_factory/__init__.py
@@ -0,0 +1,5 @@
+from add_query_node_factory import AddQueryNodeFactory
+from chat_model_node_factory import ChatModelNodeFactory
+from tool_call_node_factory import ToolCallNodeFactory
+
+__all__ = ['AddQueryNodeFactory', 'ChatModelNodeFactory', 'ToolCallNodeFactory']
